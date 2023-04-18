33800b36597cf073fef39fbfd9f7ef3bb3f82be5
ndduoc1189 <110657370+ndduoc1189@users.noreply.github.com>
2023-04-18 15:27:11 +0700

    update

--- register.js
+++ register.js
@@ -11,7 +11,7 @@ const {executablePath} = require('puppeteer')
 
 puppeteer.use(StealthPlugin())
 
-const localUser = process.env.LOCAL_USER;
+const localUser = "ndduoc"+require("shortid").generate();
 console.log(localUser);
 
 // puppeteer usage as normal
@@ -19,14 +19,14 @@ puppeteer.launch({
     headless: false,
     args: ['--lang=vi-VN'],
     executablePath: executablePath(), 
-    userDataDir:"./data/webs/1"
+    userDataDir:"./data/webs/"+localUser
 }).then(async browser => {
   console.log('Running tests..')
   const page = await browser.newPage()
   await page.goto('https://arpwallet.com/')
 
   await delay(2000);
-  const allResultsSelector = "text/Login Account";
+  const allResultsSelector = "text/Create Account";
   await page.waitForSelector(allResultsSelector);
   await page.click(allResultsSelector);
   await delay(2000);
@@ -36,16 +36,35 @@ puppeteer.launch({
   f.type(localUser)
 
   await delay(1000);
+  const email = await page.$("#email")
+  // //enter text
+  email.type(localUser+"@hhkorean.com")
+
+  await delay(1000);
+  const fname = await page.$("#fname")
+  // //enter text
+  fname.type("Nguyen Van "+localUser)
+
+  await delay(1000);
+  await page.select('#currency', 'USD')
+
+  await delay(1000);
   const log = await page.$("#password")
   log.type('tintin@123')
 
+
+
   await delay(1000);
-  const submitResultsSelector = "text/Access My Account";
+  const submitResultsSelector = "text/Complete Sign Up";
   await page.waitForSelector(submitResultsSelector);
   await page.click(submitResultsSelector);
 
+  await delay(5000);
+  const activeResultsSelector = "text/Activate My Account Here";
+  await page.waitForSelector(activeResultsSelector);
+  await page.click(activeResultsSelector);
 
-  await delay(1000);
+  await delay(10000);
   const widrawResultsSelector = "text/Withdraw Ripple (XRP)";
   await page.waitForSelector(widrawResultsSelector);
   await page.click(widrawResultsSelector);
